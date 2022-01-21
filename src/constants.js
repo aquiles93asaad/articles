4d1894927b5b0716e7fe3af2c9e8b876b935494e
aquiles93asaad <aquiles19939@gmail.com>
2022-01-21 10:01:11 -0300

    fixed the url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const url = 'http://www.pinkvilla.com';
 
 export const galleryUrl = (page) => 
-  `${url}/photo-gallery-feed-page${page ? '/' + page : ''}`;
+  `${url}/photo-gallery-feed-page${page ? '/page/' + page : ''}`;
